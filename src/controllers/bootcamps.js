76985fa0ee2455e2166dc7d3f1ed6ea2e2ebb1c0
gonzaloramosf <gonza.farinho@hotmail.com>
2022-10-27 00:20:27 -0300

    asyncHandler - slugify

--- src/controllers/bootcamps.js
+++ src/controllers/bootcamps.js
@@ -1,81 +1,57 @@
 import Bootcamp from "../models/Bootcamp.js"
 import ErrorResponse from "../utils/errorResponse.js"
+import asyncHandler from "../middlewares/async.js"
 
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-async function getAll(req, res, next) {
-    try {
-        const bootcamps = await Bootcamp.find()
-        res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps })
-
-    } catch (err) {
-        next(err)
-    }
-}
+const getAll = asyncHandler( async function(req, res, next) {
+    const bootcamps = await Bootcamp.find()
+    res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps })
+})
 
 // @desc    Get single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-async function getById(req, res, next) {
-    try {
-        const bootcamp = await Bootcamp.findById(req.params.id)
+const getById = asyncHandler( async function(req, res, next) {
+    const bootcamp = await Bootcamp.findById(req.params.id)
 
-        if (!bootcamp) {
-            return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404))
-        }
-        res.status(200).json({ success: true, data: bootcamp })
-
-    } catch (err) {
-        next(err)
+    if (!bootcamp) {
+        return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404))
     }
-}
+    res.status(200).json({ success: true, data: bootcamp })
+})
 
 // @desc    Create new bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
-async function create(req, res, next) {
-    try {
-        const bootcamp = await Bootcamp.create(req.body)
-        res.status(201).json({ success: true, data: bootcamp })
-
-    } catch (err) {
-        next(err)
-    }
-}
+const create = asyncHandler( async function(req, res, next) {
+    const bootcamp = await Bootcamp.create(req.body)
+    res.status(201).json({ success: true, data: bootcamp })
+})
 
 // @desc    Update a bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
-async function update(req, res, next) {
-    try {
-        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+const update = asyncHandler(async function(req, res, next) {
+    const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
 
-        if (!bootcamp) {
-            next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404))
-        }
-        res.status(200).json({ success: true, data: bootcamp })
-
-    } catch (err) {
-        next(err)
+    if (!bootcamp) {
+        next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404))
     }
-}
+    res.status(200).json({ success: true, data: bootcamp })
+})
 
 // @desc    Remove a bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Private
-async function remove(req, res, next) {
-    try {
-        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
+const remove = asyncHandler(async function(req, res, next) {
+    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
 
-        if (!bootcamp) {
-            next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404))
-        }
-        res.status(200).json({ success: true, data: {} })
-
-    } catch (err) {
-        next(err)
+    if (!bootcamp) {
+        next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404))
     }
-}
+    res.status(200).json({ success: true, data: {} })
+})
 
 export const Bootcamps = { getAll, getById, create, update, remove }
--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,16 +1,7 @@
-
-/*
 function asyncHandler(fn) {
-    console.log("async handler")
     return function (req, res ,next) {
         Promise.resolve(fn(req, res, next)).catch(next)
     }
 }
- */
-
-const asyncHandler = fn => (req, res, next) =>
-    Promise
-        .resolve(fn(req, res, next))
-        .catch(next)
 
 export default asyncHandler
